b02cfc37d4539a3c071cefc5b4ca64d57791eaab
quanscheng <quanscheng@163.com>
2022-06-07 00:16:25 +0800

    优化

--- src/hooks/useKeepAlive.js
+++ src/hooks/useKeepAlive.js
@@ -10,12 +10,25 @@ const KeepBox = styled.div`
 // };
 
 export const useKeepAlive = (funcArray = []) => {
-  const arrArr = funcArray.map((arr, i) => [i, arr]);
-  const map = new Map(arrArr);
+  // 方法一
+  // const arrArr = funcArray.map((arr, i) => [i, arr]);
+  // const map = new Map(arrArr);
 
   const keep = (targetFunc, boolean) => {
-    map.forEach((o, i) => map.get(i)(!boolean));
-    targetFunc(boolean);
+    // 方法一
+    // map.forEach((o, i) => map.get(i)(!boolean));
+
+    // 方法二
+    // funcArray.forEach((fn) => fn(!boolean));
+
+    // 方法三
+    funcArray.forEach((fn) => {
+      if (fn !== targetFunc) {
+        fn(!boolean);
+      } else {
+        targetFunc(boolean);
+      }
+    });
   };
 
   return { keep };
