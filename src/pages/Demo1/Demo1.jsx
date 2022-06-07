695613bb8bbcbe5e5743f21914436a14f2f5fb11
quanscheng <quanscheng@163.com>
2022-06-07 16:12:07 +0800

    ant Segmented

--- src/pages/Demo1/Demo1.jsx
+++ src/pages/Demo1/Demo1.jsx
@@ -1,44 +1,74 @@
+import "./demo1.scss";
+
 import { Avatar, Segmented } from "antd";
+import { Caption, Card, Layout, Page, Stack, TextStyle } from "@shopify/polaris";
 import React, { useState } from "react";
+
+const SegmentedItem = ({ imgsrc = "", title = "", info = "" }) => {
+  return (
+    <div style={{ padding: "4px 0" }}>
+      <Stack alignment="center">
+        <Stack.Item>
+          <Avatar
+            size="large"
+            src={imgsrc}
+            // src="https://joeschmoe.io/api/v1/random"
+            style={{ marginRight: -10 }}
+          />
+        </Stack.Item>
+        <Stack.Item>
+          <TextStyle>
+            {title} <Caption>{info}</Caption>
+          </TextStyle>
+        </Stack.Item>
+      </Stack>
+    </div>
+  );
+};
 
 const Demo1 = () => {
   const [options, setOptions] = useState([
     {
       label: (
-        <div style={{ padding: 4 }}>
-          <Avatar src="https://joeschmoe.io/api/v1/random" />
-          <div>User 1</div>
-        </div>
+        <SegmentedItem imgsrc="https://joeschmoe.io/api/v1/random" title="推特" info="张无忌" />
       ),
-      value: "user1",
+      value: "1",
     },
     {
       label: (
-        <div style={{ padding: 4 }}>
-          <Avatar style={{ backgroundColor: "#f56a00" }}>K</Avatar>
-          <div>User 2</div>
-        </div>
+        <SegmentedItem imgsrc="https://joeschmoe.io/api/v1/random" title="脸书" info="张三丰" />
       ),
-      value: "user2",
+      value: "2",
     },
     {
       label: (
-        <div style={{ padding: 4 }}>
-          <Avatar style={{ backgroundColor: "#87d068" }} />
-          <div>User 3</div>
-        </div>
+        <SegmentedItem
+          imgsrc="https://joeschmoe.io/api/v1/random"
+          title="instagram"
+          info="user1 aplex"
+        />
       ),
-      value: "user3",
+      value: "3",
     },
   ]);
 
   return (
-    <Segmented
-      options={options}
-      onChange={(value) => {
-        console.log('value: ', value);
-      }}
-    />
+    <Page title="demo 1">
+      <Layout>
+        <Layout.Section>
+          <div className="custom_Segmented">
+            <Segmented
+              // block // fill效果
+              options={options}
+              onChange={(value) => {
+                console.log("value: ", value);
+              }}
+            />
+          </div>
+          <Card sectioned>111</Card>
+        </Layout.Section>
+      </Layout>
+    </Page>
   );
 };
 
